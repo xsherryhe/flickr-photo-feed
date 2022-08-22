509bfe4f5d72290a94fda4e464234fa1d890ef33
Sherry He <xsherryhe@gmail.com>
2022-08-22 15:49:35 -0400

    Fixed bug and created more consistent styling for photo scroll arrows

--- app/javascript/photo-scroller.js
+++ app/javascript/photo-scroller.js
@@ -9,15 +9,15 @@ document.addEventListener('turbo:load', () => enablePrevNextArrows());
 
 function changePhotos(e) {
   const photos = document.querySelectorAll('.photo'),
-        currId = Number(document.querySelector('.photo:not(.hidden)').id[5]),
+        currId = Number(document.querySelector('.photo:not(.hidden)').id.slice(5)),
         upcomingId = currId + 1 * (e.target.classList.contains('previous') ? -1 : 1);
 
   photos.forEach(photo => photo.classList.add('hidden'));
-  document.querySelectorAll('.arrow').forEach(arrow => arrow.classList.add('hidden'));
+  document.querySelectorAll('.arrow').forEach(arrow => arrow.classList.add('hidden-with-space'));
 
-  document.querySelector('#photo' + upcomingId).classList.remove('hidden');
+  document.querySelector('#photo' + String(upcomingId)).classList.remove('hidden');
   if(upcomingId > 0) 
-    document.querySelector('.arrow.previous').classList.remove('hidden');
+    document.querySelector('.arrow.previous').classList.remove('hidden-with-space');
   if(upcomingId < photos.length - 1) 
-    document.querySelector('.arrow.next').classList.remove('hidden');
+    document.querySelector('.arrow.next').classList.remove('hidden-with-space');
 }
